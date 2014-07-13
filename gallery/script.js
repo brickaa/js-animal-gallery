2ffdeabc1d181caee4123ec7121daf783bd1b602
brickaa <becca.aaronson@Gmail.com>
2014-07-13 17:46:42 -0500

    DRY image toggle

--- gallery/script.js
+++ gallery/script.js
@@ -1,1 +1,37 @@
 // Add your code here
+$('.filter-button').on('click', function(e) {
+  $(this).toggleClass('active');
+  // if ($(this).attr('id') === 'monkey') {
+  //   $('.monkey').toggle();
+  // } else if ($(this).attr('id') === 'dog'){
+  //   $('.dog').toggle();
+  // } else {
+  //   $('.cat').toggle();
+  // }
+  if ($(this).hasClass('active')) {
+    showTheseImages($(this).attr('id'));
+  } else {
+    hideTheseImages($(this).attr('id'));
+  }
+  console.log($(this).attr('id'));
+});
+
+// $('#monkey').on('click', function() {
+//   $('.monkey').toggle();
+// });
+
+// $('#dog').on('click', function() {
+//   $('.dog').toggle();
+// });
+
+// $('#cat').on('click', function() {
+//   $('.cat').toggle();
+// });
+
+function hideTheseImages(imageClass) {
+  $('.' + imageClass).fadeOut();
+}
+
+function showTheseImages(imageClass) {
+  $('.' + imageClass).fadeIn();
+}
